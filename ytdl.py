e809b63df61154a3f8443b16e8ff2f075f957845
Amal Rajan <amalrajan74@gmail.com>
2017-06-02 01:25:24 +0400

    Added alpha support for downloading multiple files.

--- ytdl.py
+++ ytdl.py
@@ -3,6 +3,8 @@ import os
 
 
 class Downloader:
+    default_config = None
+    first_time = True
 
     def __init__(self):
         print("YouTube Downloader 0.1.1")
@@ -23,12 +25,22 @@ class Downloader:
         self.choice = None
         self.streams = None
         self.file_path = None
+        self.batch_file = None
         self.stream_choice = None
+        self.batch_file_path = None
+        self.default_configuration = None
 
-    def download_single(self):
-        self.url = input("\nEnter the URL of the video:\n")
+    def download_single(self, url=None, default_configuration=None):
+        if not url:
+            self.url = input("\nEnter the URL of the video:\n")
+        else:
+            self.url = url
+
         print()
-        self.video_description(self.url)
+        if default_configuration:
+            self.video_description_mini(self.url, default_configuration)
+        else:
+            self.video_description(self.url)
 
     def video_description(self, url):
         self.video = pafy.new(url.strip())
@@ -41,68 +53,88 @@ class Downloader:
         print("Views    :", self.video.viewcount)
         self.stream_lists(self.video)
 
-    def stream_lists(self, video=None):
-        print()
-        print("Select the stream: ")
-        print("-----------------")
-        print("1. Regular Streams (choose this if you're not sure)",
-              "2. Audio Streams",
-              "3. Video Streams",
-              "4. OGG Streams",
-              "5. M4 Streams",
-              "10. List all the streams", sep='\n')
-        while True:
-            self.choice = input("\nEnter your choice:\n")
-            if self.choice not in ('1', '2', '3', '4', '5', '10'):
-                print("Invalid input. Choose again.\n")
+    def video_description_mini(self, url, default_configuration):
+        self.video = pafy.new(url.strip())
+        print("[", self.video.title, "]")
+        self.stream_lists(self.video, default_configuration)
+
+    def stream_lists(self, video, default_configuration=None):
+        self.default_configuration = default_configuration
+        if not self.default_configuration:
+            print()
+            print("Select the stream: ")
+            print("-----------------")
+            print("1. Regular Streams (choose this if you're not sure)",
+                  "2. Audio Streams",
+                  "3. Video Streams",
+                  "4. OGG Streams",
+                  "5. M4 Streams",
+                  "10. List all the streams", sep='\n')
+            while True:
+                self.choice = input("\nEnter your choice:\n")
+                if self.choice not in ('1', '2', '3', '4', '5', '10'):
+                    print("Invalid input. Choose again.\n")
+                else:
+                    break
+        else:
+            if Downloader.first_time:
+                Downloader.default_config['stream_type'] = self.choice
+                Downloader.first_time = False
             else:
-                break
+                self.choice = Downloader.default_config['stream_type']
 
         eval({"1": "self.stream_regular", "2": "self.stream_audio", "3": "self.stream_video",
-              "4": "self.stream_ogg", "5": "self.stream_m4", "10": "self.stream_listall"}[self.choice] + "(video)")
+              "4": "self.stream_ogg", "5": "self.stream_m4", "10": "self.stream_listall"}[
+                 self.choice] + "(video, Downloader.default_config)")
 
-    def stream_regular(self, video):
-        print("\nAvailable Streams:\n")
-        self.streams = video.streams
-        for stream in range(len(self.streams)):
-            print(stream+1, ". ", self.streams[stream])
-        while True:
-            self.stream_choice = input("Enter your choice: ")
-            if self.stream_choice not in ('1', '2', '3', '4', '5'):
-                print("Invalid input. Choose again:\n")
-            else:
-                break
-        self.choice = input("View additional information about the stream? (y/n)\n").lower()
-        if self.choice == "y":
-            self.stream_view_additional()
-        print()
-        print("File size: ", round(self.streams[int(self.stream_choice) - 1].get_filesize() / (1024 * 1024), 2), "MB")
-
-        '''
-        Asking for location of the video to be saved into.
-        '''
-
-        while True:
-            self.file_path = input("Enter the file path:\n")
-            if os.path.exists(self.file_path):
-                break
-            else:
-                self.choice = input("The path doesn't exist. Create a new one? (y/n)")
-                if self.choice:
-                    try:
-                        os.makedirs(self.file_path)
-                    except:
-                        print("Operation failed. Couldn't create a new directory. Try again.\n")
-                    else:
-                        print("Path created successfully.\n")
+    def stream_regular(self, video, default_configuration=None):
+        if not default_configuration:
+            print("\nAvailable Streams:\n")
+            self.streams = video.streams
+            for stream in range(len(self.streams)):
+                print(stream + 1, ". ", self.streams[stream])
+            while True:
+                self.stream_choice = input("Enter your choice: ")
+                if self.stream_choice not in ('1', '2', '3', '4', '5'):
+                    print("Invalid input. Choose again:\n")
                 else:
-                    continue
+                    break
+            self.choice = input("View additional information about the stream? (y/n)\n").lower()
+            if self.choice == "y":
+                self.stream_view_additional()
+            print()
+            print("File size: ", round(self.streams[int(self.stream_choice) - 1].get_filesize() / (1024 * 1024), 2),
+                  "MB")
 
-            print("File size: ", self.streams[self.stream_choice-1].get_filesize())
-        print()
-        self.choice = input("Start download? (y/n)\n").lower()
-        if self.choice == 'y':
-            self.start_download(self.streams[int(self.stream_choice)-1], self.file_path, video)
+            '''
+            Asking for location of the video to be saved into.
+            '''
+
+            while True:
+                self.file_path = input("Enter the file path:\n")
+                if os.path.exists(self.file_path):
+                    break
+                else:
+                    self.choice = input("The path doesn't exist. Create a new one? (y/n)")
+                    if self.choice:
+                        try:
+                            os.makedirs(self.file_path)
+                        except:
+                            print("Operation failed. Couldn't create a new directory. Try again.\n")
+                        else:
+                            print("Path created successfully.\n")
+                    else:
+                        continue
+
+                print("File size: ", self.streams[self.stream_choice - 1].get_filesize())
+            print()
+            self.choice = input("Start download? (y/n)\n").lower()
+            if self.choice == 'y':
+                self.start_download(self.streams[int(self.stream_choice) - 1], self.file_path, video)
+        else:
+            self.streams = video.streams
+            self.start_download(self.streams[int(self.default_configuration['stream']) - 1],
+                                self.default_configuration['path'], video)
 
     def start_download(self, stream, path, video):
         try:
@@ -114,6 +146,32 @@ class Downloader:
         pass
 
     def download_multiple(self):
-        pass
+        while True:
+            self.batch_file_path = input("Enter the complete location of the text file: ")
+            if self.batch_file_path[-3:] != 'txt':
+                print("Invalid input. Make sure you've added the file extension along with it. Try again.\n")
+            else:
+                break
+
+        with open(self.batch_file_path) as self.batch_file:
+            self.url_data = self.batch_file.read().split('\n')
+
+        print()
+        print("URL(s) Listed: ")
+        print("---------------")
+        [print(url) for url in self.url_data]
+        print()
+
+        self.choice = input("Use different configurations for each video? (y/n, default:n)").lower()
+
+        if self.choice == 'y':
+            for url in self.url_data:
+                self.download_single(url)
+        else:
+            print("Choose the configuration for the first video, "
+                  "else all will be downloaded to at least the best quality possible, if not the preferred one.")
+            Downloader.default_config =  {"stream_type": None, "stream": None, "path": None}
+            self.download_single(self.url_data[0])
+
 
 app = Downloader()
